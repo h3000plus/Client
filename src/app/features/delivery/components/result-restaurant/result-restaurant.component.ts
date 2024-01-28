042d92031ffe948e02b86a444b51deec47335a50
noel.alam <noel.alam9999@gmail.com>
2024-01-28 23:00:19 +0600

    feat:page Connection

--- src/app/features/delivery/components/result-restaurant/result-restaurant.component.ts
+++ src/app/features/delivery/components/result-restaurant/result-restaurant.component.ts
@@ -4,15 +4,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-result-restaurant',
   templateUrl: './result-restaurant.component.html',
-  styleUrl: './result-restaurant.component.scss'
+  styleUrl: './result-restaurant.component.scss',
 })
 export class ResultRestaurantComponent {
   constructor(private router: Router) {}
 
   @Input()
-   restaurants : any = [];
+  restaurants: any = [];
 
-   handleRestaurantClick(restaurant: any) {
-    this.router.navigate(['/restaurant', restaurant?._id]);
+  handleRestaurantClick(restaurant: any) {
+    this.router.navigate(['/restaurant/', restaurant?.resId]);
   }
 }
--- src/app/features/delivery/delivery.component.ts
+++ src/app/features/delivery/delivery.component.ts
@@ -6,71 +6,57 @@ import { DeliveryService } from '../../services/delivery.service';
 @Component({
   selector: 'app-delivery',
   templateUrl: './delivery.component.html',
-  styleUrl: './delivery.component.scss'
+  styleUrl: './delivery.component.scss',
 })
 export class DeliveryComponent {
   filters = {
-    mode: "",
-    cuisine: "",
-    price: "",
-    deliveryFee: "",
-    like: ""
-  }
-  priceDisplay : boolean = false;
-  feeDisplay : boolean = false;
-  likeDisplay : boolean = false;
-
-
+    mode: '',
+    cuisine: '',
+    price: '',
+    deliveryFee: '',
+    like: '',
+  };
+  priceDisplay: boolean = false;
+  feeDisplay: boolean = false;
+  likeDisplay: boolean = false;
 
   zIndex: number = 2;
   pIndex: number = 1;
   fIndex: number = 2;
   lIndex: number = 2;
 
-  constructor(private router: Router, private deliveryService: DeliveryService) {
-
-
-  }
+  constructor(
+    private router: Router,
+    private deliveryService: DeliveryService
+  ) {}
 
   showModal: boolean = false;
-  categories = [{title: "recently viewed"}, {title: "recommended for you"}, {title: "popular"}, {title: "top 10"}]
-  restaurants : any = [];
-
+  categories = [
+    { title: 'recently viewed' },
+    { title: 'recommended for you' },
+    { title: 'popular' },
+    { title: 'top 10' },
+  ];
+  restaurants: any = [];
+  allRestaurants: any = [];
   categoryTitle: string = '';
   data: Restaurant[] = [];
   recentlyViewedData: Restaurant[] = [];
   popularData: Restaurant[] = [];
 
-  searchStatus: boolean = false;
+  searchStatus: boolean = true;
 
-ngOnInit(){
-
-  const mode = "delivery";
-  const searchTerm = "";
-  const cuisine= "";
-    this.deliveryService.filteredRestaurants({mode, searchTerm, cuisine}).subscribe( (data) => {
-    this.restaurants = data
-    console.log(this.restaurants)
-  })
-  // this.yes (mode, searchTerm, cuisine)
-}
-
-// yes (mode: string, searchTerm: string, cuisine: string) {
-
-// }
-
-
-  // handleCuisine(name: string) {
-  //   this.filters.cuisine = name;
-  //   const mode = "delivery";
-  //   const searchTerm = "";
-  //   const cuisine= name;
-  //     this.deliveryService.filteredRestaurants({mode, searchTerm, cuisine}).subscribe( (data) => {
-  //   this.restaurants = data
-  //   console.log(this.restaurants)
-  // })
-  //   // this.yes (mode, searchTerm, cuisine);
-  // }
+  ngOnInit() {
+    const mode = 'delivery';
+    const searchTerm = '';
+    const cuisine = '';
+    this.deliveryService
+      .filteredRestaurants({ mode, searchTerm, cuisine })
+      .subscribe((data) => {
+        this.restaurants = data;
+        this.allRestaurants = data;
+      });
+  }
 
   handlePrice(price: string) {
     this.filters.price = price;
@@ -87,5 +73,4 @@ ngOnInit(){
   handleSearchClick() {
     this.router.navigate(['delivery/search']);
   }
-
 }
